f39daaa42be35c92266ad5f78dafdbea5dd0d195
Jeremy Dropkin <jdropkin@andrew.cmu.edu>
2021-05-12 13:11:24 -0400

    Add basic test

--- src/main.c
+++ src/main.c
@@ -1,1 +1,21 @@
-int main(void) { return 0; }
+/**
+ * @file main.c
+ */
+
+#include <stdio.h>
+
+#include "thr.h"
+
+void *f(void *arg) {
+    (void) arg;
+    printf("Hello world\n");
+    return NULL;
+}
+
+int main(void) {
+    thr_init();
+    int tid = thr_add(f, NULL);
+    thr_wait(tid, NULL);
+    thr_finish();
+    return 0;
+}

5dc609a37a658f28e14db58cbc9f82503b477477
Jeremy Dropkin <jdropkin@andrew.cmu.edu>
2021-05-12 18:41:38 -0400

    Stealing works?

--- src/main.c
+++ src/main.c
@@ -9,8 +9,8 @@
 
 void *f(void *arg) {
     (void)arg;
+    sleep(2);
     printf("Hello world\n");
-    //sleep(1);
     return NULL;
 }
 
@@ -23,6 +23,7 @@ void *g(void *arg) {
 void *h(void *arg) {
     int i = (int)arg;
     printf("%d\n", i);
+    usleep(5000);
     return NULL;
 }
 
@@ -30,16 +31,18 @@ int main(void) {
     thr_init();
     int num_threads = 16;
     int tids[num_threads];
-    for (int i = 0; i < num_threads; i++) {
-        tids[i] = thr_add(h, (void *)i);
+    tids[0] = thr_add(f, NULL, 100);
+    for (int i = 1; i < num_threads; i++) {
+        tids[i] = thr_add(h, (void *)i, 10);
     }
 
+    thr_start();
     for (int i = 0; i < num_threads; i++) {
         thr_wait(tids[i], NULL);
     }
 
-    /*int tidf = thr_add(f, NULL);
-    int tidg = thr_add(g, NULL);
+    /*int tidf = thr_add(f, NULL, 100);
+    int tidg = thr_add(g, NULL, 1);
     thr_wait(tidf, NULL);
     thr_wait(tidg, NULL);*/
     thr_finish();

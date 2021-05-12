f365fda02011b7c80409f7d899e6e1d5c9c4ce70
Jeremy Dropkin <jdropkin@andrew.cmu.edu>
2021-05-12 15:43:07 -0400

    Stop being dumb

--- src/main.c
+++ src/main.c
@@ -3,19 +3,29 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
 
 #include "thr.h"
 
 void *f(void *arg) {
     (void)arg;
     printf("Hello world\n");
+    //sleep(1);
+    return NULL;
+}
+
+void *g(void *arg) {
+    (void)arg;
+    printf("Hello from g\n");
     return NULL;
 }
 
 int main(void) {
     thr_init();
-    int tid = thr_add(f, NULL);
-    thr_wait(tid, NULL);
+    int tidf = thr_add(f, NULL);
+    int tidg = thr_add(g, NULL);
+    thr_wait(tidf, NULL);
+    thr_wait(tidg, NULL);
     thr_finish();
     return 0;
 }
--- src/tasks.h
+++ src/tasks.h
@@ -39,10 +39,8 @@ clock_t task_cost_get_from_tid(int tid);
 
 struct task *task_create(void *(*fn)(void *), void *arg, int tid);
 
-void task_map_add(int task_id, int thread_id);
+void task_map_add(int task_id, struct task *t);
 
-void task_map_update(int task_id, int thread_id);
-
-int task_map_get_thread_id(int task_id);
+struct task *task_map_find(int task_id);
 
 #endif /* TASK_INDEX_H */

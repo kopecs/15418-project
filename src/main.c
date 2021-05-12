e6802f81ee16618348a997ef91801c96350e33b5
Jeremy Dropkin <jdropkin@andrew.cmu.edu>
2021-05-12 16:16:01 -0400

    Fix global queue insert bug

--- src/main.c
+++ src/main.c
@@ -20,12 +20,28 @@ void *g(void *arg) {
     return NULL;
 }
 
+void *h(void *arg) {
+    int i = (int)arg;
+    printf("%d\n", i);
+    return NULL;
+}
+
 int main(void) {
     thr_init();
-    int tidf = thr_add(f, NULL);
+    int num_threads = 16;
+    int tids[num_threads];
+    for (int i = 0; i < num_threads; i++) {
+        tids[i] = thr_add(h, (void *)i);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        thr_wait(tids[i], NULL);
+    }
+
+    /*int tidf = thr_add(f, NULL);
     int tidg = thr_add(g, NULL);
     thr_wait(tidf, NULL);
-    thr_wait(tidg, NULL);
+    thr_wait(tidg, NULL);*/
     thr_finish();
     return 0;
 }
